6d09db0c3cfba742e66c556a8149851fdf02c625
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2026-01-20 12:44:56 +0100

    Construct processor with studio for contrast fix

--- src/org/jens/mp4stream/MP4StreamFactory.java
+++ src/org/jens/mp4stream/MP4StreamFactory.java
@@ -1,11 +1,14 @@
 package org.jens.mp4stream;
 
+import org.micromanager.Studio;
 import org.micromanager.data.Processor;
 import org.micromanager.data.ProcessorFactory;
 
 public final class MP4StreamFactory implements ProcessorFactory {
+
    @Override
    public Processor createProcessor() {
-      return new MP4StreamProcessor();
+      Studio studio = MP4StreamPlugin.getStudio();
+      return new MP4StreamProcessor(studio);
    }
 }
